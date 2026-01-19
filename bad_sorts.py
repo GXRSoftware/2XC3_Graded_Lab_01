2cbcf6d39201558f7af3ff24b6d58ea909af9a2c
Grady Rueffer <113617132+GradyRuefferOutlook@users.noreply.github.com>
2026-01-19 16:52:19 -0500

    Graph

--- bad_sorts.py
+++ bad_sorts.py
@@ -95,3 +95,258 @@ def find_min_index(L, n):
             min_index = i
     return min_index
 
+def selection_sort2(L):
+    i = 0
+    l = len(L) - 1
+    while i < l:
+        min_index, max_index = find_indexes(L, i, l)  #LOOK HERE
+        if (i == max_index):           #LOOK HERE
+            swap(L, i, min_index)
+        else:
+            swap(L, i, min_index)
+            swap(L, l, max_index)      #LOOK HERE
+        i += 1
+        l -= 1
+
+def find_indexes(L, n, e):
+    min_index = n
+    max_index = n                  #LOOK HERE
+    for i in range(n+1, e + 1):
+        if L[i] < L[min_index]:
+            min_index = i
+        if L[i] > L[max_index]:    #LOOK HERE
+            max_index = i
+    return min_index, max_index
+
+
+
+
+
+
+
+
+###############################################################
+#                      Experiment 1:                          #
+###############################################################
+import matplotlib
+import random
+import timeit
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+lengths = [2**x for x in range(13)]
+max_value = 2**30
+lists = [create_random_list(_, max_value) for _ in lengths]
+
+n = len(lengths)
+total1, total2, total3 = 0, 0, 0
+data1, data2, data3 = [], [], []
+cmp1, cmp2, cmp3 = [], [], []
+value = random.randint(0, max_value)
+
+for _ in range(n):
+    print(_)
+    L = lists[_]
+    L1 = [*L]
+    L2 = [*L]
+    L3 = [*L]
+
+    start = timeit.default_timer()
+    bubble_sort(L1)
+    elapsed = timeit.default_timer() - start
+    total1 += elapsed
+    data1.append(elapsed)
+
+    start = timeit.default_timer()
+    insertion_sort(L2)
+    elapsed = timeit.default_timer() - start
+    total2 += elapsed
+    data2.append(elapsed)
+
+    start = timeit.default_timer()
+    selection_sort(L3)
+    elapsed = timeit.default_timer() - start
+    total3 += elapsed
+    data3.append(elapsed)
+
+plt.plot(lengths, data1, color='red', label='bubble_sort')
+plt.plot(lengths, data2, color='blue', label='insertion_sort')
+plt.plot(lengths, data3, color='lime', label='selection_sort')
+
+plt.title('Runtime Comparison of Sorting Algorithms')
+plt.xlabel('Input Size')
+plt.ylabel('Runtime')
+plt.legend()
+
+plt.show()
+
+#######
+#  2  #
+#######
+lengths = [2**x for x in range(13)]
+max_value = 2**30
+lists = [create_random_list(_, max_value) for _ in lengths]
+
+n = len(lengths)
+total1, total2, total3 = 0, 0, 0
+data1, data2, data3 = [], [], []
+cmp1, cmp2, cmp3 = [], [], []
+value = random.randint(0, max_value)
+
+for _ in range(n):
+    print(_)
+    L = lists[_]
+    L1 = [*L]
+    L2 = [*L]
+
+    start = timeit.default_timer()
+    bubble_sort(L1)
+    elapsed = timeit.default_timer() - start
+    total1 += elapsed
+    data1.append(elapsed)
+
+    start = timeit.default_timer()
+    bubble_sort2(L2)
+    elapsed = timeit.default_timer() - start
+    total2 += elapsed
+    data2.append(elapsed)
+
+plt.plot(lengths, data1, color='red', label='bubble_sort')
+plt.plot(lengths, data2, color='darkred', label='bubble_sort2')
+
+plt.title('Runtime Comparison of Sorting Algorithms')
+plt.xlabel('Input Size')
+plt.ylabel('Runtime')
+plt.legend()
+
+plt.show()
+
+lengths = [2**x for x in range(13)]
+max_value = 2**30
+lists = [create_random_list(_, max_value) for _ in lengths]
+
+n = len(lengths)
+total1, total2, total3 = 0, 0, 0
+data1, data2, data3 = [], [], []
+cmp1, cmp2, cmp3 = [], [], []
+value = random.randint(0, max_value)
+
+for _ in range(n):
+    print(_)
+    L = lists[_]
+    L1 = [*L]
+    L2 = [*L]
+
+    start = timeit.default_timer()
+    selection_sort(L1)
+    elapsed = timeit.default_timer() - start
+    total1 += elapsed
+    data1.append(elapsed)
+
+    start = timeit.default_timer()
+    selection_sort2(L2)
+    elapsed = timeit.default_timer() - start
+    total2 += elapsed
+    data2.append(elapsed)
+
+plt.plot(lengths, data1, color='lime', label='selection_sort')
+plt.plot(lengths, data2, color='green', label='selection_sort2')
+
+plt.title('Runtime Comparison of Sorting Algorithms')
+plt.xlabel('Input Size')
+plt.ylabel('Runtime')
+plt.legend()
+
+plt.show()
+
+lengths = [2**x for x in range(13)]
+max_value = 2**30
+lists = [create_random_list(_, max_value) for _ in lengths]
+
+n = len(lengths)
+total1, total2, total3 = 0, 0, 0
+data1, data2, data3 = [], [], []
+cmp1, cmp2, cmp3 = [], [], []
+value = random.randint(0, max_value)
+
+for _ in range(n):
+    print(_)
+    L = lists[_]
+    L1 = [*L]
+    L2 = [*L]
+
+    start = timeit.default_timer()
+    bubble_sort2(L1)
+    elapsed = timeit.default_timer() - start
+    total1 += elapsed
+    data1.append(elapsed)
+
+    start = timeit.default_timer()
+    selection_sort2(L2)
+    elapsed = timeit.default_timer() - start
+    total2 += elapsed
+    data2.append(elapsed)
+
+plt.plot(lengths, data1, color='darkred', label='bubble_sort2')
+plt.plot(lengths, data2, color='green', label='selection_sort2')
+
+plt.title('Runtime Comparison of Sorting Algorithms')
+plt.xlabel('Input Size')
+plt.ylabel('Runtime')
+plt.legend()
+
+plt.show()
+
+#######
+#  3  #
+#######
+
+lengths = [2**x for x in range(13)]
+max_value = 2**30
+lists = [create_near_sorted_list(_, max_value, 0) for _ in lengths]
+
+n = len(lengths)
+total1, total2, total3 = 0, 0, 0
+data1, data2, data3 = [], [], []
+cmp1, cmp2, cmp3 = [], [], []
+value = random.randint(0, max_value)
+
+for _ in range(n):
+    print(_)
+    L = lists[_]
+    L1 = [*L]
+    L2 = [*L]
+    L3 = [*L]
+
+    start = timeit.default_timer()
+    bubble_sort(L1)
+    elapsed = timeit.default_timer() - start
+    total1 += elapsed
+    data1.append(elapsed)
+
+    start = timeit.default_timer()
+    insertion_sort(L2)
+    elapsed = timeit.default_timer() - start
+    total2 += elapsed
+    data2.append(elapsed)
+
+    start = timeit.default_timer()
+    selection_sort(L3)
+    elapsed = timeit.default_timer() - start
+    total3 += elapsed
+    data3.append(elapsed)
+
+print("Binary search avg runtime (ms): ", f"{total1/n:e}")
+print("Linear search avg runtime (ms): ", f"{total2/n:e}")
+
+plt.plot(lengths, data1, color='red', label='near_sorted_bubble_sort')
+plt.plot(lengths, data2, color='blue', label='near_sorted_insertion_sort')
+plt.plot(lengths, data3, color='lime', label='near_sorted_selection_sort')
+
+plt.title('Runtime Comparison of Sorting Algorithms')
+plt.xlabel('Input Size')
+plt.ylabel('Runtime')
+plt.legend()
+
+plt.show()
